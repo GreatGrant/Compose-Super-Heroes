d4ad632f126628c8e9c8c6509650d0d36c03c797
Great Grant <greatgrant116@gmail.com>
2023-10-18 15:40:37 +0100

    fix image layout

--- app/src/main/java/com/gralliams/composesuperheroes/MainActivity.kt
+++ app/src/main/java/com/gralliams/composesuperheroes/MainActivity.kt
@@ -3,25 +3,15 @@ package com.gralliams.composesuperheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -51,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    SuperHeroesApp()
                 }
             }
         }
@@ -59,88 +49,79 @@ class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun SuperHeroesApp(){
+    fun SuperHeroesApp() {
         Scaffold(
             topBar = {
-
+                // Add a top app bar if needed
             }
-        ) { it ->
-            LazyColumn(contentPadding = it){
-                items(superHeros){
-                    HeroCard(
-                        superHero = it
-                    )
+        ) { contentPadding ->
+            LazyColumn(contentPadding = contentPadding) {
+                items(superHeros) { superHero ->
+                    HeroCard(superHero = superHero)
                 }
-
             }
-
         }
     }
 
     @Composable
-    fun HeroCard(superHero: SuperHero, modifier: Modifier = Modifier){
-
+    fun HeroCard(superHero: SuperHero, modifier: Modifier = Modifier) {
         Card(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxWidth()
                 .height(100.dp)
                 .padding(16.dp),
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 2.dp
-            )
-        ){
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-            ){
+            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 HeroPortfolio(superHero = superHero)
-                Box(
-                    modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .padding(16.dp)
-                ){
-                    Image(
-                        painter = painterResource(id = superHero.imageRes),
-                        contentDescription = "Super hero image",
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .clip(MaterialTheme.shapes.small)
-                            .padding(8.dp),
-                        contentScale = ContentScale.Crop
-                    )
-                }
+                Spacer(modifier = Modifier.weight(1f))
 
-
+                HeroImage(superHero.imageRes)
             }
         }
     }
 
     @Composable
-    fun HeroPortfolio(superHero: SuperHero){
+    fun HeroPortfolio(superHero: SuperHero) {
         Column(
-            modifier = Modifier.padding(16.dp).height(72.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-            ){
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center
+        ) {
             Text(
                 text = stringResource(id = superHero.nameRes),
                 maxLines = Int.MAX_VALUE,
-                modifier = Modifier.align(Alignment.Start),
             )
             Text(
                 text = stringResource(id = superHero.descriptionRes),
-                modifier = Modifier.align(Alignment.Start),
                 style = MaterialTheme.typography.bodyMedium
             )
         }
     }
 
+    @Composable
+    fun HeroImage(@DrawableRes heroIcon:Int, modifier: Modifier = Modifier) {
+        Box(
+            modifier = modifier
+                .size(72.dp)
+                .clip(RoundedCornerShape(8.dp))
+        ) {
+            Image(
+                painter = painterResource(heroIcon),
+                contentDescription = null,
+                alignment = Alignment.TopCenter,
+                contentScale = ContentScale.FillWidth)
+        }
+    }
+    
     @Preview(showBackground = true)
     @Composable
-    fun SuperHeroPreview(){
+    fun SuperHeroPreview() {
         ComposeSuperheroesTheme {
-//            HeroCard(superHero = SuperHeroDataSource.superHeros[0])
             SuperHeroesApp()
         }
     }
